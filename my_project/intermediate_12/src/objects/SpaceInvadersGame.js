83f0a506b03c65341f8ed3ef0582895efd4db3de
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2022-11-30 15:22:37 +0100

    fixed player postion

--- my_project/intermediate_12/src/objects/SpaceInvadersGame.js
+++ my_project/intermediate_12/src/objects/SpaceInvadersGame.js
@@ -35,17 +35,21 @@ export default class SpaceInvadersGame extends THREE.Group {
 
     /* canvas */
     //
-    const screenGeometry = new THREE.PlaneGeometry(0.864, 1.13);
-    screenGeometry.translate(0.2, 0, 0);
+    const screenWidth = 1.13;
+    const screenHeight = 0.864;
+    const screenGeometry = new THREE.PlaneGeometry(screenWidth, screenHeight);
     screenGeometry.rotateY(Math.PI / 2);
-    screenGeometry.rotateX(Math.PI / 2);
+
     screenGeometry.scale(35, 35, 35);
+    screenGeometry.translate(0.005, 0, 0);
     const screen = new THREE.Mesh(screenGeometry, corpusMaterial3);
     this.add(screen);
 
-    const playerGeometry = new THREE.PlaneGeometry(0.06, 0.06);
+    const scale = 0.15;
+    const playerSize = 0.07;
+    const playerGeometry = new THREE.PlaneGeometry(playerSize, playerSize);
+    playerGeometry.translate(0, -(screenHeight / 2 - playerSize / 2), 0);
     playerGeometry.rotateY(Math.PI / 2);
-    playerGeometry.rotateX(Math.PI / 2);
     playerGeometry.scale(35, 35, 35);
     const player = new THREE.Mesh(playerGeometry, corpusMaterial2);
     this.add(player);

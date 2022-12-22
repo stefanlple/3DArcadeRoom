17bf25dd4b47e60a57c00e2745006d997ee90f20
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2022-12-22 14:57:36 +0100

    deleted

--- intermediates-main/intermediate_25/src/eventfunctions/updateAspectRatio.js
+++
@@ -1,5 +0,0 @@
-export function updateAspectRatio() {
-  window.camera.aspect = window.innerWidth / window.innerHeight;
-  window.camera.updateProjectionMatrix();
-  window.renderer.setSize(window.innerWidth, window.innerHeight);
-}

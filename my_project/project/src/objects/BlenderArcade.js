d315c98c7beed40080b54a1ce5fa38afb1914611
Stefan Le <stefanle01@yahoo.de>
2023-01-05 23:10:25 +0100

    blenderarcade file added

---
+++ my_project/project/src/objects/BlenderArcade.js
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+import * as TWEEN from "tween";
+import Screen from "./Screen.js";
+import { RectAreaLightUniformsLib } from "../../../../lib/three.js-r145/examples/jsm/lights/RectAreaLightUniformsLib.js";
+import { RectAreaLightHelper } from "../../../../lib/three.js-r145/examples/jsm/helpers/RectAreaLightHelper.js";
+
+const loader = new GLTFLoader();
